6e4c6eddaed3b2979f74d765ccd9ead74ec8e50b
MATTHEW TYLER BARTON <mtb@MATTHEWs-MacBook-Pro.local>
2022-05-23 01:38:20 -0400

    deleted div error

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,18 @@
-'use strict'
+"use strict";
 
 // 'continue reading' buttons
 
-const continueReading = document.getElementsByClassName('.continue-reading')
-// const modal = document.getElementsByClassName("modal")
-// const span = document.getElementsByClassName("close")[0];
+const continueReading = document.getElementsByClassName(".continue-reading");
 
-function contReading() {  
-console.log("keep reading");
+function contReading() {
+  console.log("keep reading");
 }
 
-// continueReading.onclick = function() {contReading} {
-//     modal.style.display = "block";
-// }
-
-// span.onclick = function() {
-//     modal.style.display = "none";
-//   }
-
-//   window.onclick = function(event) {
-//     if (event.target == modal) {
-//       modal.style.display = "none";
-//     }
-// }
-
-// 'continue reading' modal
-
-// let modal = document.getElementsByClassName(cr-modal)
-
 // 'back to the top' button
-const backToTop = document.getElementsByClassName('.topOfPageBtn')
+const backToTop = document.getElementsByClassName(".topOfPageBtn");
 
 function topFunction() {
-console.log('test');
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  console.log("test");
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
